268622017caa903dd785174c3dd902e3fa1741f5
Franklin Oliveira <franklin.alves.oliveira@gmail.com>
2021-08-09 16:19:27 -0300

    added setup.py file to custom_tools package

---
+++ local_packages/custom_tools/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    author = "Franklin Oliveira and Marcelo Ribeiro",
+    description = "Custom module containing data wrangling, EDA and machine learning functionalities.",
+    name = "customtools",
+    version = "0.1.0",  
+    packages = find_packages(include = ['custom_tools', 'custom_tools.*']),
+    python_requires = '>=3.7',   # TODO need to add more flexibility here
+    install_requires = [         # these dependencies need to be further checked, tested and specified in future versionss
+        'impyute>=0.0.8',
+        'matplotlib>=3.0.0',
+        'numpy>=1.19.1',
+        'pandas>=0.25.3',
+        'scipy>=1.5.2',
+        'seaborn>=0.9.0',
+        'scikit-learn>=0.23.0',
+        'statsmodels>=0.11.1',
+        'xgboost>=0.90'
+    ]
+)
